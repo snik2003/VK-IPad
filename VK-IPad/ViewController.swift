b5da1ac0555ad48bc5c5b2c96510d5f34fcca63c
Sergey Nikitin <snik2003@list.ru>
2018-06-13 12:36:34 +0300

    Initial Commit

---
+++ VK-IPad/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VK-IPad
+//
+//  Created by Сергей Никитин on 13.06.2018.
+//  Copyright © 2018 Sergey Nikitin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
