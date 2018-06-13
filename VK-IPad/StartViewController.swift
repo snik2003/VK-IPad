1eddf42820881f6510c50e5d0d811575a65a74ef
Sergey Nikitin <snik2003@list.ru>
2018-06-13 14:33:06 +0300

    create LoginViewController

--- VK-IPad/StartViewController.swift
+++ VK-IPad/StartViewController.swift
@@ -12,14 +12,16 @@ class StartViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        performSegue(withIdentifier: "goLoginForm", sender: self)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
